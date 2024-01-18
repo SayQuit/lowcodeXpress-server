210d3c30be911f9d7a6fdbd737e21703dc80ae00
SayQuit <501241011@qq.com>
2024-01-18 15:27:45 +0800

    test接口

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({
   limit: '50mb',
   extended: true,
@@ -10,9 +10,17 @@ app.use(express.urlencoded({
 app.use(cors());
 
 const userRouter = require('./src/user');
-const elementJSONRouter= require('./src/elementJSON');
+const elementJSONRouter = require('./src/elementJSON');
 app.use('/user', userRouter);
 app.use('/element', elementJSONRouter);
+
+app.post('/test', async (_, res) => {
+  return res.send({
+    msg: 'success',
+    status: 200,
+    data:{}
+  })
+});
 
 app.listen(5507, () => {
   console.log('Server is running on localhost:5507');
