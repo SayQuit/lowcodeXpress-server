e3b17570687e453229873567964cf71f05080c0d
SayQuit <501241011@qq.com>
2024-01-29 06:28:53 +0800

    sql bug fixed

--- utils/sql/project/detailSQL.js
+++ utils/sql/project/detailSQL.js
@@ -2,12 +2,21 @@ const createDB = require('../../db')
 const { formatTime } = require('../../time')
 
 async function selectJSON(account, id) {
-    const selectSQL = `select createAt, id, name, description, type, tech, lib, lastModified element from project where account = '${account}' and id = '${id}'`
+    const selectSQL = `select createAt, id, name, description, type, tech, lib, lastModified, element from project where account = '${account}' and id = '${id}'`
     const db = await createDB()
     return new Promise((resolve, reject) => {
         db.query(selectSQL, (error, results) => {
             if (error || results.length !== 1) reject(error)
             else {
+        console.log(
+            {
+                ...results[0],
+                lib: JSON.parse(results[0].lib),
+                element: JSON.parse(results[0].element),
+                createAt: formatTime(results[0].createAt),
+                lastModified: formatTime(results[0].lastModified),
+            }
+        );
                 resolve({
                     ...results[0],
                     lib: JSON.parse(results[0].lib),
