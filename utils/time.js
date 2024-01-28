8221975399708bdf6e6dffcdc610ac2ee5340667
SayQuit <501241011@qq.com>
2024-01-29 06:21:48 +0800

    日期格式化

---
+++ utils/time.js
@@ -0,0 +1,13 @@
+const formatTime = (timestamp) => {
+    const now = new Date(Number(timestamp));
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+module.exports = { formatTime }
