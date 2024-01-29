8e60864553520ffc90913fe8d30bb6d262cfbd4a
SayQuit <501241011@qq.com>
2024-01-29 16:56:36 +0800

    fix sql error

--- utils/sql/project/createSQL.js
+++ utils/sql/project/createSQL.js
@@ -2,7 +2,7 @@ const createDB = require('../../db')
 
 async function insertJSON(account, element, id, name, description, type, tech, lib, dragGroup) {
     const time = new Date().getTime()
-    const insertSQL = `insert into project(account, element, id, createAt, lastModified, name, description, type, tech, lib, dragGroup) values ('${account}','${JSON.stringify(element)}','${id}','${time}','${time}','${name}','${description}','${type}','${tech}','${JSON.stringify(lib)}',''${JSON.stringify(dragGroup)})`
+    const insertSQL = `insert into project(account, element, id, createAt, lastModified, name, description, type, tech, lib, dragGroup) values ('${account}','${JSON.stringify(element)}','${id}','${time}','${time}','${name}','${description}','${type}','${tech}','${JSON.stringify(lib)}','${JSON.stringify(dragGroup)}')`
     const db = await createDB()
     return new Promise((resolve, reject) => {
         db.query(insertSQL, (error, result) => {
