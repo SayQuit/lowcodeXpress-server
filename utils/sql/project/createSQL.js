0256254e1d04608602e6929292884ae315bafa5a
SayQuit <501241011@qq.com>
2024-02-27 23:24:43 +0800

    解决创建页面sql bug

--- utils/sql/project/createSQL.js
+++ utils/sql/project/createSQL.js
@@ -2,7 +2,7 @@ const createDB = require('../../db')
 
 async function insertJSON(account, element, id, name, description, type, tech, lib, variable, event) {
     const time = new Date().getTime()
-    const insertSQL = `insert into project(account, element, id, createAt, lastModified, name, description, type, tech, lib, variable) values ('${account}','${JSON.stringify(element)}','${id}','${time}','${time}','${name}','${description}','${type}','${tech}','${JSON.stringify(lib)}','${JSON.stringify(variable)}','${JSON.stringify(event)}')`
+    const insertSQL = `insert into project(account, element, id, createAt, lastModified, name, description, type, tech, lib, variable, event) values ('${account}','${JSON.stringify(element)}','${id}','${time}','${time}','${name}','${description}','${type}','${tech}','${JSON.stringify(lib)}','${JSON.stringify(variable)}','${JSON.stringify(event)}')`
     const db = await createDB()
     return new Promise((resolve, reject) => {
         db.query(insertSQL, (error, result) => {
