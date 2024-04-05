384fb12398d785683b139d678cbdf9a095a933f3
SayQuit <501241011@qq.com>
2024-04-05 12:49:15 +0800

    数据格式化

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/export', exportRouter);
 
 const latestDrawing = [{ "id": "1", "archicasca": { "url": "http://localhost:5507/1.svg", "type": "svg", "o_url": "http://localhost:5507/1.svg" }, "download_url": "http://localhost:5507/1.svg", "o_download_url": "http://localhost:5507/1.svg" }, { "id": "2", "archicasca": { "url": "http://localhost:5507/2.svg", "type": "svg", "o_url": "http://localhost:5507/2.svg" }, "download_url": "http://localhost:5507/2.svg", "o_download_url": "http://localhost:5507/2.svg" }, { "id": "3", "archicasca": { "url": "http://localhost:5507/3.svg", "type": "svg", "o_url": "http://localhost:5507/3.svg" }, "download_url": "http://localhost:5507/3.svg", "o_download_url": "http://localhost:5507/3.svg" }]
 const quiz = [ { "archiId": "1", "correct": 0, "count": 0, "name": "黄埔军校", "url": "http://localhost:5507/icon1.svg" }, { "archiId": "2", "correct": 0, "count": 0, "name": "起义路", "url": "http://localhost:5507/icon1.svg" }, { "archiId": "3", "correct": 0, "count": 0, "name": "北京路", "url": "http://localhost:5507/icon1.svg" }, { "archiId": "4", "correct": 0, "count": 0, "name": "人民公园", "url": "http://localhost:5507/icon1.svg" }, ]
-const drawingStatistics = [ { "archiId": "1", "name": "中山纪念堂", "painting": 120, "url": "http://localhost:5507/icon1.svg" }, { "archiId": "2", "name": "农讲所纪念馆", "painting": 130, "url": "http://localhost:5507/icon1.svg" }, { "archiId": "3", "name": "镇海楼", "painting": 460, "url": "http://localhost:5507/icon1.svg" }, { "archiId": "3", "name": "广州塔", "painting": 240, "url": "http://localhost:5507/icon1.svg" }, ]
+const drawingStatistics = [ { "archiId": "1", "name": "中山纪念堂", "painting": 0, "url": "http://localhost:5507/icon1.svg" }, { "archiId": "2", "name": "农讲所纪念馆", "painting": 0, "url": "http://localhost:5507/icon1.svg" }, { "archiId": "3", "name": "镇海楼", "painting": 0, "url": "http://localhost:5507/icon1.svg" }, { "archiId": "3", "name": "广州塔", "painting": 0, "url": "http://localhost:5507/icon1.svg" }, ]
 const chartQuizCorrect = [{ "count": 0, "name": "人民公园", }, { "count": 0, "name": "镇海楼", }, { "count": 0, "name": "农讲所纪念馆", }, { "count": 0, "name": "中山纪念堂", }, { "count": 0, "name": "人民公园", },]
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -39,13 +39,13 @@ app.get('/test', async (_, res) => {
   latestDrawing[1] = latestDrawing[2]
   latestDrawing[2] = a
   quiz.forEach((item) => {
-    item.count += getRandomInt(0, 20)
+    item.count += getRandomInt(0, 5)
   })
   drawingStatistics.forEach((item) => {
-    item.painting += getRandomInt(0, 20)
+    item.painting += getRandomInt(0, 5)
   })
   chartQuizCorrect.forEach((item) => {
-    item.count += getRandomInt(0, 20)
+    item.count += getRandomInt(0, 5)
   })
   return res.send({
     msg: 'success',
